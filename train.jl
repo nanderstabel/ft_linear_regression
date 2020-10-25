8ae3a87ce75b47a01d02c105802730be91534b99
nanderstabel <nanderstabel@gmail.com>
2020-10-25 13:18:24 +0100

    Add animated graphs

--- train.jl
+++ train.jl
@@ -1,40 +1,61 @@
 using CSV
 using DataFrames
 using Plots
+using Statistics
 
 # Read and standardize data
 data = CSV.read("data.csv", DataFrame)
 stats = describe(data)[!, [:variable, :mean, :min, :max]]
 stats.std = [std(data.km), std(data.price)]
 km = (data[:, :km] .- stats.mean[1]) ./ stats.std[1]
-price = (data[:, :price] .- stats.mean[2]) ./ stats.std[2]
-# km = (data[:, :km] .- stats.min[1]) ./ (stats.max[1] - stats.min[1])
-# price = (data[:, :price] .- stats.min[2]) ./ (stats.max[2] - stats.min[2])
-# km = (data[:, :km])
-# price = (data[:, :price])
-minmax
+price = (data[:, :price])
+
 # Formulæ
 𝚺(x) = sum(x)
 estimatePrice(km) = θ₀ + (θ₁ * km)
-y(x) = θ₀ + θ₁ * x
+h(x, θ₀, θ₁) = θ₀ + (θ₁ * x)
+J(θ₀, θ₁) = 1 / (2 * m) * 𝚺((map(estimatePrice, km) - price).^2)
 
 # Initialise hyperparamters
 θ₀ = 0.0
 θ₁ = 0.0
-α = 500
-epochs = 250
+α = 0.1
+epochs = 100
 m = size(data)[1]
 
-println("start")
-for epoch in 1:epochs
-    tmpθ₀ = α * (1 / m) * 𝚺(map(estimatePrice, km) - price)
-    tmpθ₁ = α * (1 / m) * 𝚺((map(estimatePrice, km) - price) .* km)
-    θ₀ -= tmpθ₀
-    θ₁ -= tmpθ₁
-    println(epoch, ", ", θ₀, θ₁)
+function unscale(θ₀, θ₁)
+    θ₀ = θ₀ - θ₁ * (stats.mean[1] / stats.std[1])
+    θ₁ = θ₁ / stats.std[1]
+    return θ₀, θ₁
 end
 
+function get_regline(scaled=false, (θ₀, θ₁)=(θ₀, θ₁))
+    regline = scatter(
+            scaled ? km : data.km,
+            data.price,
+            xlabel = "km",
+            ylabel = "price");
+        plot!(
+            x -> h(x, θ₀, θ₁),
+            scaled ? minimum(km) : stats.min[1],
+            scaled ? maximum(km) : stats.max[1])
+    return regline
+end
 
-gr()
-scatter(km, price); plot!(x -> y(x), minimum(km), maximum(km))
-# scatter(data.km, data.price); plot!(x -> y(x), stats.min[1], stats.max[1])
+function animate_graphs()
+    regline = get_regline(false, unscale(θ₀, θ₁))
+    scaled_regline = get_regline(true)
+    plot(regline, scaled_regline, legend=false)
+end
+
+anim = @animate for epoch in 1:epochs
+    tmpθ₀ = α * (1 / m) * 𝚺(map(estimatePrice, km) - price)
+    tmpθ₁ = α * (1 / m) * 𝚺((map(estimatePrice, km) - price) .* km)
+    global θ₀ -= tmpθ₀
+    global θ₁ -= tmpθ₁
+    cost = J(θ₀, θ₁)
+    animate_graphs()
+    println(cost)
+end
+
+gif(anim, "graphs.gif", fps = 15)
